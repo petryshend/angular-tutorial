a1359a666dde9e774310272477a1a589054e7ac8
petryshend <petryshend@gmail.com>
2018-08-19 20:25:27 +0300

    routing start

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Nikolay'},
+  {id: 12, name: 'Andrey'},
+  {id: 13, name: 'Pyer'},
+  {id: 14, name: 'Natasha'},
+  {id: 15, name: 'M. Burien'},
+  {id: 16, name: 'Maria'},
+  {id: 17, name: 'Gosudar'},
+  {id: 18, name: 'Bonaparte'},
+  {id: 19, name: 'Anatol'},
+];
